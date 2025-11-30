d51c35074f98b2ea47d4cfb636216af74e521905
Budge42 <jack@budge.co.nz>
2025-11-30 15:29:08 +1300

    Remove service worker for GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2241047993"
+    serviceWorkerVersion: "1287737908"
   }
 });
